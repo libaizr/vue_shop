161bfa1ec4c2b84d086c08094924e870dadb5d50
libaizr <1578291009@qq.com>
2021-01-05 13:36:45 +0800

    表单验证以及重置按钮

--- src/views/Login/LoginChildren/LoginInput.vue
+++ src/views/Login/LoginChildren/LoginInput.vue
@@ -4,15 +4,21 @@
     <avatar></avatar>
     <!-- 登陆表单 -->
     <!-- 用户名 -->
-    <el-form :model="LoginForm" label-width="80px" class="login_form">
-      <el-form-item label="用户名">
+    <el-form
+      ref="loginFromRef"
+      :model="LoginForm"
+      :rules="loginFormRules"
+      label-width="80px"
+      class="login_form"
+    >
+      <el-form-item label="用户名" prop="username">
         <el-input
           v-model="LoginForm.username"
           prefix-icon="el-icon-user-solid"
         ></el-input>
       </el-form-item>
       <!-- 密码 -->
-      <el-form-item label="密码">
+      <el-form-item label="密码" prop="password">
         <el-input
           v-model="LoginForm.password"
           prefix-icon="el-icon-view"
@@ -22,7 +28,7 @@
       <!-- 按钮 -->
       <el-form-item class="btns">
         <el-button type="primary">登录</el-button>
-        <el-button type="info">重置 </el-button>
+        <el-button type="info" @click="resetLoginForm">重置 </el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -31,7 +37,7 @@
 <script>
 import Avatar from "./Avatar";
 
-import { reactive, toRefs } from "vue";
+import { reactive, toRefs, getCurrentInstance } from "vue";
 
 export default {
   name: "LoginInput",
@@ -39,18 +45,40 @@ export default {
     Avatar,
   },
   setup() {
+    const instance = getCurrentInstance();
+
     const data = reactive({
       // 登录表单数据绑定对象
       LoginForm: {
         username: "",
         password: "",
       },
+
+      // 表单验证规则
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在3到10个字符", trigger: "blur" },
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 15, message: "长度在6到15个字符", trigger: "blur" },
+        ],
+      },
     });
+
+    const resetLoginForm = () => {
+      console.log(instance);
+      instance.refs.loginFromRef.resetFields();
+    };
 
     const refData = toRefs(data);
 
     return {
       ...refData,
+      resetLoginForm,
     };
   },
 };
