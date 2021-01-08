f97efa80187277b613610611e7f20865d1d52566
libaizr <1578291009@qq.com>
2021-01-08 19:43:19 +0800

    用户操作返回数据到后台

--- src/components/content/HomePages/Users/Users.vue
+++ src/components/content/HomePages/Users/Users.vue
@@ -10,45 +10,163 @@
       <template #header>
         <div class="clearfix">
           <span>用户列表</span>
-          <el-button style="float: right; padding: 3px 0" type="text"
-            >操作按钮</el-button
-          >
         </div>
       </template>
-      <div v-for="(item, id) in userList" :key="id" class="text item">
-        {{ item.role_name }}
+      <div>
+        <el-table :data="userList" stripe style="width: 100%">
+          <el-table-column type="index" width="50" label="#"> </el-table-column>
+          <el-table-column prop="role_name" label="名称" width="180">
+          </el-table-column>
+          <el-table-column prop="username" label="用户名" width="180">
+          </el-table-column>
+          <el-table-column prop="mobile" label="手机"> </el-table-column>
+          <el-table-column prop="email" label="邮箱"> </el-table-column>
+          <el-table-column prop="mg_state" label="状态">
+            <template v-slot="scope">
+              <el-switch
+                v-model="scope.row.mg_state"
+                active-color="#13ce66"
+                inactive-color="#eeee98"
+                @change="userStateChange(scope.row)"
+              >
+              </el-switch>
+              <!-- {{scope.row.mg_state}} -->
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" width="180px">
+            <template v-slot="scope">
+              <!-- 修改按钮 -->
+              <el-tooltip
+                class="item"
+                effect="light"
+                content="修改"
+                placement="bottom"
+                :enterable="false"
+              >
+                <el-button
+                  type="primary"
+                  icon="el-icon-edit"
+                  size="mini"
+                  @click="changeuser"
+                ></el-button>
+              </el-tooltip>
+
+              <!-- 删除按钮 -->
+              <el-tooltip
+                class="item"
+                effect="light"
+                content="删除"
+                placement="bottom"
+                :enterable="false"
+              >
+                <el-button
+                  type="danger"
+                  icon="el-icon-delete"
+                  size="mini"
+                ></el-button>
+              </el-tooltip>
+
+              <!-- 分配角色按钮 -->
+              <el-tooltip
+                class="item"
+                effect="light"
+                content="分配角色"
+                placement="bottom"
+                :enterable="false"
+              >
+                <el-button type="warning" icon="el-icon-setting" size="mini">
+                </el-button>
+              </el-tooltip>
+              {{ scope.row.id }}
+            </template>
+          </el-table-column>
+        </el-table>
       </div>
-      {{ userList }}
+      <!-- {{ userList }} -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="pagenum"
+        :page-sizes="[1, 2, 4]"
+        :page-size="pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      >
+      </el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
 import { getUsersList } from "network/getUsersList";
+import { putUserState } from "network/putUserState";
 import { reactive, toRefs } from "vue";
+import { ElMessage } from "element-plus";
 
 export default {
   name: "Users",
   setup() {
-    getUsersList(1, 10)
-      .then((res) => {
-        console.log(res.data);
-        if (res.data.meta.status !== 200) return console.log(res.data.meta.msg);
-        return (data.userList = res.data.data.users);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    const getList = () => {
+      getUsersList(data.pagenum, data.pagesize)
+        .then((res) => {
+          console.log(res.data);
+          if (res.data.meta.status !== 200)
+            return console.log(res.data.meta.msg);
+          data.total = res.data.data.total;
+          data.userList = res.data.data.users;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
 
     const data = reactive({
+      total: 0,
       userList: [],
-      pagenum: "",
+      pagenum: 1,
+      pagesize: 2,
     });
+    getList();
+
+    // 设置单页显示条数
+    const handleSizeChange = (newSize) => {
+      // console.log(newSize);
+      data.pagesize = newSize;
+      getList();
+    };
+
+    // 设置当前页码
+    const handleCurrentChange = (newPage) => {
+      // console.log(newPage);
+      data.pagenum = newPage;
+      getList();
+    };
+
+    const userStateChange = (userInfo) => {
+      console.log(userInfo);
+      putUserState(userInfo.id, userInfo.mg_state).then((res) => {
+        if (res.data.meta.status != 200) {
+          userInfo.mg_state = !userInfo.mg_state;
+          console.log(res);
+          return ElMessage.error("更新用户状态失败");
+        } else {
+          ElMessage.success("更新用户状态成功");
+        }
+      });
+    };
+
+    const changeuser = (item) => {
+      console.log(item);
+    };
 
     const refData = toRefs(data);
 
     return {
       ...refData,
+      changeuser,
+      handleSizeChange,
+      handleCurrentChange,
+      userStateChange,
     };
   },
 };
@@ -76,7 +194,7 @@ export default {
 }
 
 .box-card {
-  width: 70%;
+  width: 75%;
   position: relative;
   top: 50%;
   left: 50%;
--- src/network/request.js
+++ src/network/request.js
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 export function request(config) {
   const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8888/api/private/v1/',
+    baseURL: 'http://127.0.0.1:8888/api/private/v1',
     timeout: 6000
   })
 
   instance.interceptors.request.use((config) => {
-    console.log(config);
+    // console.log(config);
     config.headers.Authorization = sessionStorage.getItem('token')
     return config
   }, (error) => {
@@ -15,6 +15,7 @@ export function request(config) {
   })
 
   // instance.interceptors.response.use((response) => {
+  //   // response.headers.Authorization = sessionStorage.getItem('token')
   //   return response.data
   // }, (error) => {
   //   console.log('拦截器报错');
